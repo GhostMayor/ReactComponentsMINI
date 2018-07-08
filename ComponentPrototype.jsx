6ecbd91982de617989f2913a872e26384c77572a
GhostMayor <prok-uror@yandex.ru>
2018-07-09 02:12:22 +0300

    fix: Proto get||set

--- ComponentPrototype.jsx
+++ ComponentPrototype.jsx
@@ -5,11 +5,21 @@ class Proto extends React.Component{
       super(props);
   
       this.state = {
-        value: props.value || null
+        value: props.value || null,
+        width: props.width || null,
+        height: props.height || null,
       }
   
       this.getValue = this.getValue.bind(this);
       this.editValue = this.editValue.bind(this);
+    }
+
+    get value(){
+      return this.state.value
+    }
+
+    set value(new_value){
+      this.setState({value:new_value})
     }
   
     getValue(){
