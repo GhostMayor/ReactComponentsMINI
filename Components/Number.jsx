de70d6f14fb312105d8638e02e9a7cb7d0cbd0d7
GhostMayor <prok-uror@yandex.ru>
2018-07-09 23:15:07 +0300

    add NUmber

---
+++ Components/Number.jsx
@@ -0,0 +1,17 @@
+import Proto from 'ComponenPrototype';
+
+class Number extends Proto{
+    constructor(props){
+      super(props);
+    }
+  
+    render(){
+      return this.edit ? 
+        <div>{this.caption}<input value={this.value} onChange={(e)=>{
+          this.value = parseInt(e.target.value) || 0
+          }}/></div> : <span>{this.caption}:{this.value}</span>
+    }
+  }
+
+
+export default NUmber
